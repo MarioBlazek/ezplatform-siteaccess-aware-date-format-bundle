bafb84ad58033f465537ec9993ff4aa616ba861c
Mario Blažek <blazek.mario@gmail.com>
2019-05-29 08:54:52 +0200

    Enable timezone handling

--- bundle/Twig/Extension/DateTimeFormatterExtension.php
+++ bundle/Twig/Extension/DateTimeFormatterExtension.php
@@ -8,6 +8,8 @@ use DateTimeInterface;
 use Marek\SiteAccessAwareDateFormatBundle\Core\SiteAccessAwareFormatter;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\Environment;
+use Twig\Extension\CoreExtension;
 
 final class DateTimeFormatterExtension extends AbstractExtension
 {
@@ -29,20 +31,33 @@ final class DateTimeFormatterExtension extends AbstractExtension
     public function getFilters(): array
     {
         return [
-            new TwigFilter('m_datetime', [$this, 'format']),
+            new TwigFilter('m_datetime', [$this, 'format'], ['needs_environment' => true]),
         ];
     }
 
     /**
      * Calls date formatter.
      *
+     * @param \Twig\Environment $environment
      * @param \DateTimeInterface $date
      * @param string $format
+     * @param string|null $timezone
      *
      * @return string
      */
-    public function format(DateTimeInterface $date, $format = 'default'): string
+    public function format(Environment $environment, DateTimeInterface $date, $format = 'default', $timezone = null): string
     {
+        if (null === $timezone) {
+            $timezone = $environment->getExtension(CoreExtension::class)->getTimezone();
+        } elseif (!$timezone instanceof \DateTimeZone) {
+            $timezone = new \DateTimeZone($timezone);
+        }
+
+        $date = clone $date;
+        if (false !== $timezone) {
+            $date->setTimezone($timezone);
+        }
+
         return $this->formatter->format($date, $format);
     }
 }
